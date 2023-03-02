c657d015dfada35270bd2a6e1296dd745b01a6cb
buju heatonadegbile <masterbuju@yahoo.com>
2023-03-02 14:32:27 +0000

    add day 8 code

--- 100 Days of Python.py
+++ 100 Days of Python.py
@@ -607,3 +607,46 @@ while end_of_game == False:
         print("You Win!")
 
     print(stages[lives])
+
+#############
+### DAY 8 ###
+#############
+
+# FUNCTIONS WITH INPUTS
+
+def greet(name):
+    print(f"Hello {name}")
+    print("Hello")
+    print("Hello")
+
+greet("buju")
+
+# POSITIONAL ARGUMENTS
+
+def greet_with(name, location):
+    print(f"My name is {name}.")
+    print(f"I live in {location}.")
+
+greet_with("Buju", "London")
+
+# KEYWORD ARGUMENTS
+
+def greet_with(name, location):
+    print(f"My name is {name}.")
+    print(f"I live in {location}.")
+
+greet_with(location = "London", name = "Buju")
+
+# PAINT AREA CALCULATOR
+
+import math
+
+def paint_calc(height, width, cover):
+    area = (height * width) 
+    num_of_cans = math.ceil(area/cover)
+    print(f"You'll need {num_of_cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
